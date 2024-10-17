cf4c8e17cfa696f92916bdf47b880a2bb9412527
Benio-sama <vago.kata.viki@gmail.com>
2024-10-17 14:22:40 +0200

    kesz mukodik es designolt is
    nice

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Body, Controller, Get, Post, Render, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { newForgalasDto } from './newFoglalas.dto';
+import { Response } from 'express';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   @Render('index')
@@ -11,5 +13,74 @@ export class AppController {
     return {
       message: this.appService.getHello()
     };
+  }
+
+  @Get('/foglalas')
+  @Render('foglalas')
+  foglalas() {
+    return {
+      errors: [],
+      data: {}
+    }
+  }
+
+  @Post('/foglalas')
+  foglalasPost(@Body() foglalas: newForgalasDto, @Res() response: Response) {
+
+    const errors: string[] = [];
+
+    if (!foglalas.nev) {
+      errors.push('Nev megadasa kotelezo!');
+    }
+
+    if (!foglalas.email) {
+      errors.push('Az email megadasa kotelezo!');
+    }
+
+    if (!foglalas.datum) {
+      errors.push('A datum megadasa kotelezo!');
+    }
+
+    if (!foglalas.nezok) {
+      errors.push('A nezok szamanak a megadasa kotelezo!');
+    }
+
+    if (foglalas.nev == undefined || foglalas.nev == null || foglalas.nev == '' || foglalas.nev.trim().length == 0) {
+      errors.push('A nev nem lehet space vagy barmilyen mas whitespace!');
+    }
+
+    if (! /^[a-z.-_]{1,}@[a-z]{1,}.[a-z]{2,}$/.test(foglalas.email.toLowerCase())) {
+      errors.push('Az email nem megfeleloen van megadva!');
+    }
+
+    if (Date.parse(foglalas.datum) < Date.now()) {
+      errors.push('A mai datumnal korabbi datumot nem lehet megadni!')
+    }
+
+    if (isNaN(parseInt(foglalas.nezok))) {
+      errors.push('A nezok szama nem szam!');
+    }
+
+    if (parseInt(foglalas.nezok) < 1 && parseInt(foglalas.nezok) > 10) {
+      errors.push('A nezok szama nem lehet kisebb 1-nel es nagyobb 10-nel!');
+    }
+
+    if (errors.length > 0) {
+      return response.render('foglalas',
+        {
+          errors,
+          data: foglalas
+        }
+      )
+    } else {
+      return response.redirect('/success');
+    }
+
+  }
+
+  @Get('/success')
+  @Render('success')
+  success() {
+
   }
 }
---
+++ src/newFoglalas.dto.ts
@@ -0,0 +1,6 @@
+export class newForgalasDto {
+    nev: string;
+    email: string;
+    datum: string;
+    nezok: string;
+}
